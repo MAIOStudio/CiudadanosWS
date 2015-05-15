805cbd3d0f782e6979d31f043216867887875c23
Dear Martinez <dearmartinez@icloud.com>
2015-05-14 21:36:56 -0400

    Updating database data

--- target/generated-sources/annotations/entities/Ciudadano_.java
+++ target/generated-sources/annotations/entities/Ciudadano_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-05-14T11:52:15")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-05-14T21:35:42")
 @StaticMetamodel(Ciudadano.class)
 public class Ciudadano_ { 
 
